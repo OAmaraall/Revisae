eebe15f98906b9d981420761babf32e44e0919f7
OAmaraall <konzinhoo@gmail.com>
2026-05-27 00:40:29 -0300

    chore: update base path to /Revisae/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Anotae/',
+    base: '/Revisae/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
